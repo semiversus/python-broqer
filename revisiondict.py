58cda2d78e93d9a49ee710704283b34b5fae9b39
Günther Jena <guenther@jena.at>
2018-03-23 00:34:24 +0100

    added doctest to revisiondict

--- revisiondict.py
+++ revisiondict.py
@@ -1,10 +1,52 @@
-"""Provides a way to get dictionary changes since a given revision."""
+"""
+RevisionDict works like an ordinary dictionary with additional revision
+keeping of changes. Basically the following attribute and two methods are
+important:
+
+* .revision - returning the actual revision as integer (starting with 0)
+* .key_to_revision(key) - return the revision when the given key was changed
+* .checkout(start=0) - return a dict with changes older than `start`
+
+>>> d=RevisionDict()
+>>> d.revision                    # get revision (is 0 at init)
+0
+
+Adding new items:
+>>> d['a']=0; d['b']=1; d['c']=2  # make three updates
+>>> d.revision                    # showing 3 changes
+3
+
+Inspecting content of RevisionDict:
+>>> d.checkout()=={'c': 2, 'b': 1, 'a': 0} # get a dictionary with all changes
+True
+>>> d.checkout(2)                 # get all changes starting with rev. 2
+{'c': 2}
+>>> d.checkout(3)                 # all changes starting with actual revision
+{}
+>>> d.key_to_revision('b')        # revision where 'b' was changed last time
+2
+>>> d
+RevisionDict([_Item(key='a', value=0, revision=1), \
+_Item(key='b', value=1, revision=2), \
+_Item(key='c', value=2, revision=3)])
+
+Update items:
+>>> d['b']=3                      # update value of 'b' (was 2 before)
+>>> d.revision
+4
+>>> d.key_to_revision('b')
+4
+>>> d.checkout(3)                 # get all changes starting with rev. 3
+{'b': 3}
+>>> tuple(d.keys())               # iterate over keys (they are sorted by rev.)
+('a', 'c', 'b')
+"""
 
 __author__="Günther Jena"
 
 import collections
 import bisect
-import itertools
+
 
 class _Item(collections.namedtuple('_Item', 'key value revision')):
   """ _Item representing a key:value pair with information about the revision
@@ -15,33 +57,39 @@ class _Item(collections.namedtuple('_Item', 'key value revision')):
     """__lt__ method is used for bisect"""
     return self.revision<other.revision
 
+
 class RevisionDict(collections.MutableMapping):
   def __init__(self):
     self._items=list() # keeping _Item objects, guaranteed sorted by revision
     self._key_to_index=dict() # dict indexing position of key in self._items
-    self._actual_revision=1
+    self._actual_revision=0 # number of actual revision
     
   def __setitem__(self, key, value):
+    """ set value for given key and update the actual revision """
+    self._actual_revision+=1
     if key in self._key_to_index:
       # if this key already available remove entry from self._items
       del self[key]
     self._key_to_index[key]=len(self._items) # indexing the end of self._items
     self._items.append(_Item(key, value, self._actual_revision))
-    self._actual_revision+=1
     
   def __getitem__(self, key):
+    """ return value to the given key """
     return self._items[self._key_to_index[key]].value
   
   def __delitem__(self, key):
+    """ remove key from this collection """
     index=self._key_to_index.pop(key) # get (and remove) index for this key
     self._items.pop(index) # remove that item entry
     self._key_to_index.update( # update indicies for keys
       ((k,i-1) for (k,i) in self._key_to_index.items() if i>index) )
   
   def __iter__(self):
+    """ returns a iterator over the keys sorted by revision """
     return iter( sorted(self._key_to_index, key=self._key_to_index.get) )
 
   def __len__(self):
+    """ return number of items """
     return len(self._items)
   
   def key_to_revision(self, key):
@@ -49,7 +97,7 @@ class RevisionDict(collections.MutableMapping):
     return self._items[self._key_to_index[key]].revision
     
   def checkout(self, start=None):
-    """ Get a dict() with all changes from revision `start` on """
+    """ get a dict() with all changes from revision `start` on """
     if start is not None:
       start_index=bisect.bisect(self._items, _Item(None, None, start))
     else:
@@ -63,3 +111,7 @@ class RevisionDict(collections.MutableMapping):
   def revision(self):
     """ get latest revision """
     return self._actual_revision
+
+if __name__=='__main__':
+  import doctest
+  doctest.testmod()
