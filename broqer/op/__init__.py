edfe983358adc18071bd4dbcfd6b9996a25dc5ad
Günther Jena <guenther@jena.at>
2018-04-09 21:58:36 +0200

    added switch operator

--- broqer/op/__init__.py
+++ broqer/op/__init__.py
@@ -6,6 +6,7 @@ from .map_async import MapAsync, map_async
 from .sample import Sample, sample
 from .sink import Sink, sink
 from .sliding_window import SlidingWindow, sliding_window
+from .switch import Switch, switch
 from .to_future import ToFuture, to_future
 from .update_dict import UpdateDict, update_dict
 
---
+++ broqer/op/switch.py
@@ -0,0 +1,26 @@
+from typing import Any, List
+
+from broqer import Publisher
+
+from ._operator import Operator, build_operator
+
+
+class Switch(Operator):
+  def __init__(self, selection_publisher, publisher_mapping:List[Publisher]):
+    Operator.__init__(self, selection_publisher)
+    self._selection_publisher=selection_publisher
+    self._selected_publisher=None
+    self._mapping=publisher_mapping
+
+  def emit(self, *args:Any, who:Publisher) -> None:
+    if who==self._selection_publisher:
+      if args[0]!=self._selected_publisher:
+        if self._selected_publisher:
+          self._selected_publisher.unsubscribe(self)
+        self._selected_publisher=args[0]
+        self._mapping[self._selected_publisher].subscribe(self)
+    else:
+      assert who==self._mapping[self._selected_publisher], 'emit from not selected publisher'
+      self._emit(*args)
+
+switch=build_operator(Switch)
