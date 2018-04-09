8639a84fab14cff06dac0874c5d3705efee0e58b
Günther Jena <guenther@jena.at>
2018-04-09 19:45:15 +0200

    added subject to broqer.__init__; work on comments

--- broqer/subject.py
+++ broqer/subject.py
@@ -6,5 +6,4 @@ from broqer import Disposable, Publisher, Subscriber
 class Subject(Publisher, Subscriber):
   def emit(self, *args:Any) -> None:
     for subscription in tuple(self._subscriptions):
-      # TODO: critical place to think about handling exceptions
       subscription.emit(*args, who=self)
