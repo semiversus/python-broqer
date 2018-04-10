c867f59f8d507c311120b394a58e4728d3afdc29
Günther Jena <guenther@jena.at>
2018-04-11 01:00:04 +0200

    add partial functionality to op.sink

--- broqer/op/sink.py
+++ broqer/op/sink.py
@@ -1,4 +1,5 @@
 from typing import Any, Callable, Optional
+from functools import partial
 
 from broqer import Disposable, Publisher, Subscriber
 
@@ -6,8 +7,12 @@ from ._operator import build_operator
 
 
 class Sink(Subscriber, Disposable):
-  def __init__(self, publisher:Publisher, sink_function:Callable[[Any], None]):
-    self._sink_function=sink_function
+  def __init__(self, publisher:Publisher, sink_function:Callable[[Any], None], *args, **kwargs):
+    if args or kwargs:
+        self._sink_function=partial(sink_function, *args, **kwargs)
+    else:
+        self._sink_function=sink_function
+
     self._disposable=publisher.subscribe(self)
   
   def emit(self, *args:Any, who:Optional[Publisher]=None):
