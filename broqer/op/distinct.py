5f344382815b4de2acafdd9fe62cc357156b37a1
Günther Jena <guenther@jena.at>
2018-04-11 01:33:36 +0200

    improved op.distinct

--- broqer/op/distinct.py
+++ broqer/op/distinct.py
@@ -6,12 +6,17 @@ from ._operator import Operator, build_operator
 
 class Distinct(Operator):
   def emit(self, *args:Any, who:Publisher) -> None:
+    assert len(args)>=1, 'need at least one argument for distinct'
     if args!=getattr(self, '_cache', None):
       self._cache=args
       self._emit(*args)
   
   @property
   def cache(self):
-    return self._cache
+    cache=getattr(self, '_cache', (None,))
+    if len(cache)==1:
+      return cache[0]
+    else:
+      return cache
 
 distinct=build_operator(Distinct)
--- test/test_op_distinct.py
+++ test/test_op_distinct.py
@@ -3,14 +3,54 @@ from broqer import Publisher, Subject
 from broqer.op import distinct, sink, cache
 import mock
 
+def test_cache_distinct():
+  s=Subject()
+  cb=mock.Mock()
+
+  dut=s|cache(0)|distinct()
+  dut|sink(cb)
+
+  cb.assert_called_once_with(0)
+  assert dut.cache==0
+
+  cb.reset_mock()
+
+  s.emit(0.0)
+  assert dut.cache==0
+  cb.assert_not_called()
+
+  s.emit(False) # False==0
+  assert dut.cache==0
+  cb.assert_not_called()
+
+  s.emit(1)
+  assert dut.cache==1
+  cb.assert_called_once_with(1)
+
+  s.emit(1,2)
+  assert dut.cache==(1,2)
+  cb.assert_called_with(1,2)
+
 def test_distinct():
   s=Subject()
   cb=mock.Mock()
 
-  s|cache(0)|distinct()|sink(cb)
+  dut=s|distinct()
+  dut|sink(cb)
 
-  cb.assert_called_once_with(0)
-  cb.reset_mock()
+  cb.assert_not_called()
+  assert dut.cache==None
 
   s.emit(0.0)
+  assert dut.cache==0
+  cb.assert_called_once_with(0)
+
+  cb.reset_mock()
+
+  s.emit(False) # False==0
+  assert dut.cache==0
   cb.assert_not_called()
+
+  s.emit(1)
+  assert dut.cache==1
+  cb.assert_called_once_with(1)
