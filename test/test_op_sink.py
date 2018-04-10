151c54bb870288ac6c321d889830637b06f5a997
Günther Jena <guenther@jena.at>
2018-04-11 01:59:55 +0200

    extended test for op.sink

--- test/test_op_sink.py
+++ test/test_op_sink.py
@@ -60,3 +60,38 @@ def test_sink_on_subscription():
 
     s.emit(1)
     assert not cb.called
+
+def test_sink_partial():
+    cb=mock.Mock()
+
+    s=Subject()
+    sink_instance=Sink(s, cb, 1, 2, 3, a=1)
+    assert isinstance(sink_instance, Disposable)
+
+    assert not cb.called
+    assert len(s)==1
+
+    # test various emits on source
+    s.emit()
+    cb.assert_called_with(1, 2, 3, a=1)
+
+    s.emit(None)
+    cb.assert_called_with(1, 2, 3, None, a=1)
+
+    s.emit(1)
+    cb.assert_called_with(1, 2, 3, 1, a=1)
+
+    s.emit(1,2)
+    cb.assert_called_with(1, 2, 3, 1,2, a=1)
+    
+    s.emit( (1,2) )
+    cb.assert_called_with(1, 2, 3, (1,2), a=1)
+
+    # testing dispose()
+    cb.reset_mock()
+
+    sink_instance.dispose()
+    assert len(s)==0
+
+    s.emit(1)
+    assert not cb.called
