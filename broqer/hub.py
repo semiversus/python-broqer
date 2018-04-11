aee29c799cda771dea726461075b4d503d2feb9d
Günther Jena <guenther@jena.at>
2018-04-12 01:58:22 +0200

    rewrote hub.py

--- broqer/hub.py
+++ broqer/hub.py
@@ -1,93 +1,40 @@
-from broqer.stream import Stream
+from broqer import Publisher, Subscriber
 from collections import defaultdict
-from typing import Optional, Union, Any
-from types import MappingProxyType
+from typing import Any, Optional
 
 SEP='.' # separator used for hierarchy
 
-class AssignedStream(Stream):
-  def __init__(self, hub:'Hub', name:str):
-    Stream.__init__(self)
-    self._hub=hub
-    self._name=name
-
-  @property
-  def hub(self):
-    return self._hub
+class ProxyPublisher(Publisher, Subscriber):
+  def assign(self, publisher:Publisher):
+    publisher.subscribe(self)
   
-  @property
-  def name(self):
-    return self._name
-
-  @property
-  def path(self):
-    return self._hub._prefix()+self._name
+  def emit(self, *args:Any, who:Optional[Publisher]=None) -> None:
+    self._emit(*args)
  
 class Hub:
-  def __init__(self, parent_hub:Optional['Hub']=None, name:str=''):
-    self._name=name
-    self._parent_hub=parent_hub
-    self._streams=dict()
-    self._hubs=dict()
+  def __init__(self):
+    self._publishers=defaultdict(ProxyPublisher)
   
-  @property
-  def hubs(self) -> dict:
-    return MappingProxyType(self._hubs)
+  def __setitem__(self, path:str, publisher:Publisher):
+    if path in self._publishers:
+      raise KeyError('A publisher already assigned to %r'%path)
+    self._publishers[path].assign(publisher)
   
-  @property
-  def streams(self, stream_name) -> dict:
-    return MappingProxyType(self._streams)
+  def __getitem__(self, path:str) -> ProxyPublisher:
+    return self._publishers[path]
 
-  @property
-  def parent_hub(self):
-    return self._parent_hub
-  
-  @property
-  def name(self):
-    return self._name
-  
-  @property
-  def path(self):
-    if self._parent_hub:
-      return self.parent_hub._prefix() + self._name
-    elif self._name:
-      return self._name
-    else:
-      return ''
-  
-  def _prefix(self):
-    if self._parent_hub:
-      return self._parent_hub._prefix()+self._name+SEP
-    elif self._name:
-      return self._name+SEP
-    else:
-      return ''
-  
   def __iter__(self):
-    for stream in self._streams.values():
-      yield stream.path
-    for hub in self._hubs.values():
-      yield from iter(hub)
+    return self._publishers.__iter__()
 
-  def __getitem__(self, stream_name:str) -> Union[AssignedStream, 'Hub']:
-    if SEP in stream_name:
-      # if SEP is found get the Hub and do __getitem__ there
-      hub_name, stream_path=stream_name.split(sep=SEP, maxsplit=1)
-      assert hub_name not in self._streams, 'Could not access as Hub as %r is used for a Stream'%hub_name
-      if hub_name not in self._hubs:
-        self._hubs[hub_name]=Hub(parent_hub=self, name=hub_name)
-      return self._hubs[hub_name][stream_path]
-    elif stream_name in self._hubs: # if it's a stream hub
-      return self._hubs[stream_name]
-    else:
-      if stream_name not in self._streams:
-        self._streams[stream_name]=AssignedStream(self, stream_name)
-      return self._streams[stream_name]
-
-  def unsubscribe_all(self) -> None:
-    for stream in self._streams.values():
-      stream.unsubscribe_all()
-    for hub in self._hubs.values():
-      hub.unsubscribe_all()
-    self._hubs.clear()
-    self._streams.clear()
+class SubordinateHub:
+  def __init__(self, hub, prefix):
+    self._hub=hub
+    self._prefix=prefix
+  
+  def __setitem__(self, path:str, publisher:Publisher):
+    if self.prefix+path in self._hub._publishers:
+      raise KeyError('A publisher already assigned to %r'%path)
+    self._publishers[self._prefix+path].assign(publisher)
+  
+  def __getitem__(self, path:str) -> ProxyPublisher:
+    return self._publishers[self._prefix+path]
--- examples/example1.py
+++ examples/example1.py
@@ -5,15 +5,10 @@ from broqer import op
 
 hub=Hub()
 
-def log(prefix):
-  def _(msg):
-    print('%s: "%s"'%(prefix, msg))
-  return _
+message=hub['a.b.message']
 
-message=hub['a.b.message'].setup('Start')
-
-message|op.sink(log('Sink1')) # client2 is subscribing (returns a disposable - here not used)
-message.emit('Example doing subscribe and then propose') # client1 is emiting a new value
-message|op.sink(log('Sink2'))
-
-hub.unsubscribe_all() # reset hub (clear all streams)
+message.emit('First Emit')
+message|op.sink(print, 'Sink1')
+message.emit('Second Emit')
+message|op.sink(print, 'Sink2')
+message.emit('Third Emit')
