dcd8d9bcdec485e5b5b4b912e7741309a264947a
Günther Jena <guenther@jena.at>
2018-04-12 08:07:44 +0200

    more work on hub

--- broqer/hub.py
+++ broqer/hub.py
@@ -5,26 +5,42 @@ from typing import Any, Optional
 SEP='.' # separator used for hierarchy
 
 class ProxyPublisher(Publisher, Subscriber):
-  def assign(self, publisher:Publisher):
-    publisher.subscribe(self)
-  
+  def __init__(self):
+    Publisher.__init__(self)
+    self._publisher=None
+
   def emit(self, *args:Any, who:Optional[Publisher]=None) -> None:
+    if who is not None and who!=self._publisher:
+      raise TypeError('Emit from unassigned publisher. Please use |hub[\'...\'] style for assignment')
     self._emit(*args)
+  
+  def __call__(self, publisher:Publisher) -> 'ProxyPublisher':
+    if self._publisher is not None:
+      raise ValueError('ProxyPublisher is already assigned')
+    self._publisher=publisher
+    return self
+  
+  @property
+  def assigned(self):
+    return self._publisher is not None
+  
+  @property
+  def meta(self):
+    return getattr(self, '_meta', None)
+  
+  @meta.setter
+  def meta(self, meta_dict):
+    if hasattr(self, '_meta'):
+      raise ValueError('ProxyPublisher already has a meta dict')
+    self._meta=meta_dict
+  
  
 class Hub:
   def __init__(self):
     self._publishers=defaultdict(ProxyPublisher)
   
-  def __setitem__(self, path:str, publisher:Publisher):
-    if path in self._publishers:
-      raise KeyError('A publisher already assigned to %r'%path)
-    self._publishers[path].assign(publisher)
-  
   def __getitem__(self, path:str) -> ProxyPublisher:
     return self._publishers[path]
-
-  def __iter__(self):
-    return self._publishers.__iter__()
 
 class SubordinateHub:
   def __init__(self, hub, prefix):
--- examples/example1.py
+++ examples/example1.py
@@ -1,14 +1,23 @@
 # showing simple subscription to streams
 
 from broqer.hub import Hub
-from broqer import op
+from broqer import op, Subject
 
 hub=Hub()
 
 message=hub['a.b.message']
+s=Subject()
 
 message.emit('First Emit')
 message|op.sink(print, 'Sink1')
 message.emit('Second Emit')
 message|op.sink(print, 'Sink2')
 message.emit('Third Emit')
+
+print(message.assigned)
+s|message
+s.emit('Test')
+print(message.assigned)
+
+for p,s in hub:
+  print(p,s)
