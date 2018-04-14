adeff4df7a54fcbb46efeef0dea6bf751bf5ad5a
Günther Jena <guenther@jena.at>
2018-04-15 00:39:55 +0200

    fixed .cache property in op.distinct and make it emit on subscription

--- broqer/op/distinct.py
+++ broqer/op/distinct.py
@@ -1,22 +1,33 @@
-from typing import Any
+from typing import Any, Optional
 
 from broqer import Publisher, Subscriber, SubscriptionDisposable
 
 from ._operator import Operator, build_operator
 
 class Distinct(Operator):
+  def __init__(self, publisher:Publisher, *start_values:Optional[Any]=None):
+    Operator.__init__(self, publisher)
+    self._cache=start_values
+
+  def subscribe(self, subscriber:Subscriber) -> SubscriptionDisposable:
+    disposable=super().subscribe(subscriber)
+    if self._cache is not None:
+      subscriber.emit(*self._cache, who=self)
+    return disposable
+
   def emit(self, *args:Any, who:Publisher) -> None:
     assert len(args)>=1, 'need at least one argument for distinct'
-    if args!=getattr(self, '_cache', None):
+    if args!=self._cache:
       self._cache=args
       self._emit(*args)
   
   @property
   def cache(self):
-    cache=getattr(self, '_cache', (None,))
-    if len(cache)==1:
-      return cache[0]
+    if self._cache is None:
+      return None
+    if len(self._cache)==1:
+      return self._cache[0]
     else:
-      return cache
+      return self._cache
 
 distinct=build_operator(Distinct)
