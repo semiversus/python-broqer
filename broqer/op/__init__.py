0c4ba6d92a6b49d69f55667ee5ec665b93eaab2e
Günther Jena <guenther@jena.at>
2018-04-15 01:11:03 +0200

    check for asyncio availability

--- broqer/op/__init__.py
+++ broqer/op/__init__.py
@@ -1,22 +1,28 @@
 from .accumulate import Accumulate, accumulate
 from .cache import Cache, cache
 from .combine_latest import CombineLatest, combine_latest
-from .debounce import Debounce, debounce
-from .delay import Delay, delay
 from .distinct import Distinct, distinct
 from .filter import Filter, filter
 from .from_iterable import FromIterable
-from .from_polling import FromPolling
 from .map import Map, map
-from .map_async import MapAsync, map_async, Mode
 from .pluck import Pluck, pluck
 from .reduce import Reduce, reduce
-from .sample import Sample, sample
 from .sink import Sink, sink
 from .sliding_window import SlidingWindow, sliding_window
 from .switch import Switch, switch
-from .throttle import Throttle, throttle
-from .to_future import ToFuture, to_future
+
+try:
+  import asyncio # load following operators only on asyncio availability
+except:
+  pass
+else:
+  from .debounce import Debounce, debounce
+  from .delay import Delay, delay
+  from .from_polling import FromPolling
+  from .sample import Sample, sample
+  from .map_async import MapAsync, map_async, Mode
+  from .throttle import Throttle, throttle
+  from .to_future import ToFuture, to_future
 
 # TODO operators
 # timeout - emit (a value) when timeout on source has passed
