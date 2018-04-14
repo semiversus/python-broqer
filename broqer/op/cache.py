8751019fc30b8ffce66113b2cd3f7291e206936e
Günther Jena <guenther@jena.at>
2018-04-15 00:39:07 +0200

    fix .cache property in op.cache

--- broqer/op/cache.py
+++ broqer/op/cache.py
@@ -8,6 +8,7 @@ from ._operator import Operator, build_operator
 class Cache(Operator):
   def __init__(self, publisher:Publisher, *start_values:Any):
     Operator.__init__(self, publisher)
+    assert len(start_values)>=1, 'need at least one argument for cache start_values'
     self._cache=start_values
 
   def subscribe(self, subscriber:Subscriber) -> SubscriptionDisposable:
@@ -21,7 +22,10 @@ class Cache(Operator):
   
   @property
   def cache(self):
-    return self._cache
+    if len(self._cache)==1:
+      return self._cache[0]
+    else:
+      return self._cache
 
 cache=build_operator(Cache)
 
