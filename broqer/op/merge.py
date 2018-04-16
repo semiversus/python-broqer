9c4b40320c3146f672f8f58521f6dafc608b5d20
Günther Jena <guenther@jena.at>
2018-04-16 20:51:44 +0200

    added Merge operator

---
+++ broqer/op/merge.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+from broqer import Publisher, Subscriber, SubscriptionDisposable
+
+from ._operator import MultiOperator, build_operator
+
+
+class Merge(MultiOperator):
+  def __init__(self, *publishers:Publisher):
+    Operator.__init__(self, *publisher)
+
+  def emit(self, *args:Any, who:Publisher) -> None:
+    assert who in self._publishers, 'emit comming from non assigned publisher'
+    self._emit(self._cache)
+
+merge=build_operator(Merge)
