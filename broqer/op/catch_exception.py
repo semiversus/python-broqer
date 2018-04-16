308d2686c77752afebcd4b45749672dee86ab726
Günther Jena <guenther@jena.at>
2018-04-16 20:52:24 +0200

    added CatchException operator

---
+++ broqer/op/catch_exception.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from broqer import Publisher
+
+from ._operator import Operator, build_operator
+
+
+class CatchException(Operator):
+  def __init__(self, publisher:Publisher, *exceptions):
+    Operator.__init__(self, publisher)
+    self._exceptions=exceptions
+
+  def emit(self, *args:Any, who:Publisher) -> None:
+    assert who==self._publisher, 'emit comming from non assigned publisher'
+    try:
+      self._emit(*args)
+    except self._exceptions:
+      pass
+
+catch_exception=build_operator(CatchException)
