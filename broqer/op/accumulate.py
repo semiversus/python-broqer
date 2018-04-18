b295ce828c72cb2522a7b2203a695d35bb113821
Günther Jena <guenther@jena.at>
2018-04-18 23:35:32 +0200

    op.Accumulate: added state property, pep8, type hints, doctests

--- broqer/op/accumulate.py
+++ broqer/op/accumulate.py
@@ -1,3 +1,35 @@
+"""
+Call a function with state and new value which is returning new state and value to emit.
+
+Usage:
+>>> from broqer import Subject, op
+>>> s = Subject()
+
+>>> def moving_average(state, value):
+...     state=state[1:]+[value]
+...     return state, sum(state)/len(state)
+
+>>> lowpass = s | op.accumulate(moving_average, init=[0, 0, 0])
+>>> lowpass | op.sink(print)
+<...>
+>>> s.emit(3)
+1.0
+>>> s.emit(3)
+2.0
+>>> s.emit(3)
+3.0
+>>> s.emit(3)
+3.0
+>>> lowpass.state
+[3, 3, 3]
+
+Reseting (or just setting) the state is also possible:
+
+>>> lowpass.reset([1, 1, 1])
+>>> s.emit(4)
+2.0
+
+"""
 from typing import Any, Callable
 
 from broqer import Publisher
@@ -6,18 +38,25 @@ from ._operator import Operator, build_operator
 
 
 class Accumulate(Operator):
-  def __init__(self, publisher:Publisher, func:Callable[[Any], Any], init):
-    Operator.__init__(self, publisher)
-    self._acc_func=func
-    self._state=init
+    def __init__(self, publisher: Publisher, func: Callable[[Any, Any], Any],
+                 init) -> None:
+        Operator.__init__(self, publisher)
+        self._acc_func = func
+        self._state = init
 
-  def reset(self, init):
-    self._state=init
+    def reset(self, state):
+        self._state = state
     
-  def emit(self, arg:Any, who:Publisher) -> None:
-    assert who==self._publisher, 'emit comming from non assigned publisher'
-    self._state, result=self._acc_func(self._state, arg)
-    self._emit(result)
+    @property
+    def state(self):
+        return self._state
+
+    def emit(self, *args: Any, who: Publisher) -> None:
+        assert len(args) == 1, \
+            'reduce is only possible for emits with single argument'
+        assert who == self._publisher, 'emit from non assigned publisher'
+        self._state, result = self._acc_func(self._state, args[0])
+        self._emit(result)
 
 
-accumulate=build_operator(Accumulate)
+accumulate = build_operator(Accumulate)
