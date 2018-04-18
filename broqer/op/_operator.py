26708223298e63586cff166a77e0997b5d912dbd
Günther Jena <guenther@jena.at>
2018-04-19 00:32:19 +0200

    op._operator: documentation, pep8, type hints, doctests

--- broqer/op/_operator.py
+++ broqer/op/_operator.py
@@ -1,48 +1,65 @@
-import asyncio
-from typing import Any, List
-
 from broqer import Publisher, Subscriber, SubscriptionDisposable
 
 
 class Operator(Publisher, Subscriber):
-  def __init__(self, publisher:Publisher):
-    super().__init__()
-    self._publisher=publisher
+    """ Base class for operators depending on a single publisher. This
+    publisher will be subscribed as soon as this operator is subscribed the
+    first time.
 
-  def subscribe(self, subscriber:'Subscriber') -> SubscriptionDisposable:
-    disposable=Publisher.subscribe(self, subscriber)
-    if len(self._subscriptions)==1: # if this was the first subscription 
-      self._publisher.subscribe(self)
-    return disposable
-  
-  def unsubscribe(self, subscriber:'Subscriber') -> None:
-    Publisher.unsubscribe(self, subscriber)
-    if not self._subscriptions:
-      self._publisher.unsubscribe(self)
+    On unsubscription of the last subscriber the depentent publisher will also
+    be unsubscripted.
+    """
+    def __init__(self, publisher: Publisher) -> None:
+        super().__init__()
+        self._publisher = publisher
+
+    def subscribe(self, subscriber: Subscriber) -> SubscriptionDisposable:
+        disposable = Publisher.subscribe(self, subscriber)
+        if len(self._subscriptions) == 1:  # if this was the first subscription
+            self._publisher.subscribe(self)
+        return disposable
+
+    def unsubscribe(self, subscriber: Subscriber) -> None:
+        Publisher.unsubscribe(self, subscriber)
+        if not self._subscriptions:
+            self._publisher.unsubscribe(self)
 
 
 class MultiOperator(Publisher, Subscriber):
-  def __init__(self, *publishers:List[Publisher]):
-    super().__init__()
-    self._publishers=publishers
+    """ Base class for operators depending on multiple publishers. Like
+    Operator all publishers will be subscribed on first subscription to this
+    operator. Accordingly all publishers get unsubscribed on unsubscription
+    of the last subscriber.
+    """
+    def __init__(self, *publishers: Publisher) -> None:
+        super().__init__()
+        self._publishers = publishers
 
-  def subscribe(self, subscriber:'Subscriber') -> SubscriptionDisposable:
-    disposable=Publisher.subscribe(self, subscriber)
-    if len(self._subscriptions)==1: # if this was the first subscription
-      for _publisher in self._publishers: # subscribe to all dependent publishers
-        _publisher.subscribe(self)
-    return disposable
-  
-  def unsubscribe(self, subscriber:'Subscriber') -> None:
-    Publisher.unsubscribe(self, subscriber)
-    if not self._subscriptions:
-      for _publisher in self._publishers:
-        _publisher.unsubscribe(self)
+    def subscribe(self, subscriber: Subscriber) -> SubscriptionDisposable:
+        disposable = Publisher.subscribe(self, subscriber)
+        if len(self._subscriptions) == 1:  # if this was the first subscription
+            for _publisher in self._publishers:
+                # subscribe to all dependent publishers
+                _publisher.subscribe(self)
+        return disposable
+
+    def unsubscribe(self, subscriber: Subscriber) -> None:
+        Publisher.unsubscribe(self, subscriber)
+        if not self._subscriptions:
+            for _publisher in self._publishers:
+                _publisher.unsubscribe(self)
 
 
 def build_operator(operator_cls):
-  def _op(*args, **kwargs):
-    def _build(publisher):
-      return operator_cls(publisher, *args, **kwargs)
-    return _build
-  return _op
+    """ This function is taking a operator class and is returning a function
+    to be called when used in a pipeline. _op function is a closure to add
+    arguments to the class initialisation.
+
+    The resulting function takes a publisher as argument and returns a new
+    publisher corresponding the operator functionality.
+    """ 
+    def _op(*args, **kwargs):
+        def _build(publisher):
+            return operator_cls(publisher, *args, **kwargs)
+        return _build
+    return _op
