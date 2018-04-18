8785447fa32b184deaee5097df63b27aa72000db
Günther Jena <guenther@jena.at>
2018-04-18 23:51:22 +0200

    op.CatchException: pep8, type hints, doctests

--- broqer/op/catch_exception.py
+++ broqer/op/catch_exception.py
@@ -1,3 +1,31 @@
+"""
+Catching exceptions of following operators in the pipelines
+
+Usage:
+>>> from broqer import Subject, op
+>>> s = Subject()
+
+Example with exception:
+>>> disposable = s | op.pluck(0) | op.sink(print)
+
+>>> s.emit([1,2,3])
+1
+>>> s.emit([])
+Traceback (most recent call last):
+...
+IndexError: list index out of range
+
+>>> disposable.dispose()
+
+Now with ``catch_exception``:
+>>> s | op.catch_exception(IndexError) | op.pluck(0) | op.sink(print)
+<...>
+
+>>> s.emit([1,2,3])
+1
+>>> s.emit([]) # will cause a exception but will be catched
+
+"""
 from typing import Any
 
 from broqer import Publisher
@@ -6,16 +34,17 @@ from ._operator import Operator, build_operator
 
 
 class CatchException(Operator):
-  def __init__(self, publisher:Publisher, *exceptions):
-    Operator.__init__(self, publisher)
-    self._exceptions=exceptions
+    def __init__(self, publisher: Publisher, *exceptions) -> None:
+        assert len(exceptions) >= 1, 'need at least one exception'
+        Operator.__init__(self, publisher)
+        self._exceptions = exceptions
 
-  def emit(self, *args:Any, who:Publisher) -> None:
-    assert who==self._publisher, 'emit comming from non assigned publisher'
-    try:
-      self._emit(*args)
-    except self._exceptions:
-      pass
+    def emit(self, *args: Any, who: Publisher) -> None:
+        assert who == self._publisher, 'emit from non assigned publisher'
+        try:
+            self._emit(*args)
+        except self._exceptions:
+            pass
 
 
-catch_exception=build_operator(CatchException)
+catch_exception = build_operator(CatchException)
