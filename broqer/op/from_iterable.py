3313f89687ccfe87b576b351cda3a0f46e949244
Günther Jena <guenther@jena.at>
2018-04-25 23:07:26 +0200

    op.from_iterable: add documentation with doctests

--- broqer/op/from_iterable.py
+++ broqer/op/from_iterable.py
@@ -1,3 +1,23 @@
+"""
+Use an iterable and emit each value
+
+Usage:
+>>> from broqer import op
+>>> l = [1, 2, 3]
+>>> s = op.FromIterable(l)
+
+>>> _disposable = s | op.sink(print)
+1
+2
+3
+
+>>> l.append(4)
+>>> _disposable2 = s | op.sink(print, 'Second sink:')
+Second sink: 1
+Second sink: 2
+Second sink: 3
+Second sink: 4
+"""
 from broqer import Publisher, Subscriber, SubscriptionDisposable
 
 
@@ -9,5 +29,5 @@ class FromIterable(Publisher):
     def subscribe(self, subscriber: Subscriber) -> SubscriptionDisposable:
         disposable = Publisher.subscribe(self, subscriber)
         for v in self._iterable:
-            self._emit(v)
+            subscriber.emit(v, who=self)
         return disposable
