d71105f9d6f9f685f33d260d4d1dddab13338077
Günther Jena <guenther@jena.at>
2018-04-25 23:43:06 +0200

    op.merge: add documentation with doctests

--- broqer/op/merge.py
+++ broqer/op/merge.py
@@ -1,3 +1,17 @@
+"""
+Merge emits of multiple publishers into one stream
+
+Usage:
+>>> from broqer import Subject, op
+>>> s1 = Subject()
+>>> s2 = Subject()
+
+>>> _d = s1 | op.merge(s2) | op.sink(print, 'Merge:')
+>>> s1.emit(1)
+Merge: 1
+>>> s2.emit('abc')
+Merge: abc
+"""
 from typing import Any
 
 from broqer import Publisher
