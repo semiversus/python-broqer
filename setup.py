1e4ffaf2a6c6b4a7c9a7d1d9346002fd5011b647
Günther Jena <guenther@jena.at>
2018-04-27 01:31:50 +0200

    fixing url in setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https: //github.com/semiversus/python-broqer',
+    url='https://github.com/semiversus/python-broqer',
     version='0.1.0',
     zip_safe=False,
 )
