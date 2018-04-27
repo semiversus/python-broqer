80c7b9a02fc60870206da7553d3bf33d987f41db
Günther Jena <guenther@jena.at>
2018-04-27 02:36:38 +0200

    Bump version: 0.1.2 → 0.1.3

--- broqer/__init__.py
+++ broqer/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Günther Jena'
 __email__ = 'guenther@jena.at'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from .core import Disposable, SubscriptionDisposable, Subscriber, Publisher
 from .subject import Subject, Value
