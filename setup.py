563e123ddfc5cd73cc9fc4e949e9a4ef50e6285b
Günther Jena <guenther@jena.at>
2018-04-29 23:19:17 +0200

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/semiversus/python-broqer',
-    version='0.1.4',
+    version='0.1.5',
     zip_safe=False,
 )
