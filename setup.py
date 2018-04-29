e3c4c9ff6c1bb3f292cab581f5d7312f3bf514ce
Günther Jena <guenther@jena.at>
2018-04-29 22:44:52 +0200

    fixed find_packages in setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     include_package_data=True,
     keywords='broker publisher subscriber reactive',
     name='broqer',
-    packages=find_packages(include=['broqer']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
