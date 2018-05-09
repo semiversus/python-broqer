9fcf09304b584b07c3117c6e0f45bee37e21808d
Günther Jena <guenther@jena.at>
2018-05-10 00:48:09 +0200

    added .reset() method to op.Throttle

--- broqer/op/throttle.py
+++ broqer/op/throttle.py
@@ -5,7 +5,8 @@ Usage:
 >>> import asyncio
 >>> from broqer import Subject, op
 >>> s = Subject()
->>> _d = s | op.throttle(0.1) | op.sink(print)
+>>> throttle_publisher = s | op.throttle(0.1)
+>>> _d = throttle_publisher | op.sink(print)
 >>> s.emit(1)
 1
 >>> s.emit(2)
@@ -13,6 +14,14 @@ Usage:
 >>> s.emit(3)
 >>> asyncio.get_event_loop().run_until_complete(asyncio.sleep(0.2))
 3
+
+It's also possible to reset the throttling duration
+>>> s.emit(4)
+4
+>>> s.emit(5)
+>>> asyncio.get_event_loop().run_until_complete(asyncio.sleep(0.05))
+>>> throttle_publisher.reset()
+5
 """
 import asyncio
 from typing import Any, Tuple  # noqa: F401
@@ -52,6 +61,11 @@ class Throttle(Operator):
                 self._duration, self._wait_done_cb)
         else:
             self._call_later_handler = None
+
+    def reset(self):
+        if self._call_later_handler is not None:
+            self._call_later_handler.cancel()
+            self._wait_done_cb()
 
 
 throttle = build_operator(Throttle)
