a8443235b766e193aec3ccbe8fe2f8222826a7fd
Günther Jena <guenther@jena.at>
2018-06-21 09:48:51 +0200

    Bump version: 0.3.3-dev → 0.3.4-dev

--- broqer/__init__.py
+++ broqer/__init__.py
@@ -8,7 +8,7 @@ from .default_error_handler import default_error_handler
 
 __author__ = 'Günther Jena'
 __email__ = 'guenther@jena.at'
-__version__ = '0.3.3-dev'
+__version__ = '0.3.4-dev'
 
 __all__ = [
     'StatefulPublisher', 'Disposable', 'Publisher', 'Subscriber',
--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/semiversus/python-broqer',
-    version='0.3.3-dev',
+    version='0.3.4-dev',
     zip_safe=False,
 )
