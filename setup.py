5a9ceef3f24f6eb642ce77b6c2fb0171a9a7d4a1
Günther Jena <guenther@jena.at>
2018-06-21 09:36:19 +0200

    Bump version: 0.3.2-dev → 0.3.3-dev

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/semiversus/python-broqer',
-    version='0.3.2-dev',
+    version='0.3.3-dev',
     zip_safe=False,
 )
