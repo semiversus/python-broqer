d542386456ec7ff25a84ae49ce64d3f4b2e3654f
Günther Jena <guenther@jena.at>
2018-06-28 23:32:54 +0200

    added topic_mapper.py

---
+++ broqer/op/topic_mapper.py
@@ -0,0 +1,14 @@
+from typing import MutableMapping, Any
+from broqer import Subscriber, Publisher
+from broqer.hub import Topic
+
+class TopicMapper(Subscriber):
+    def __init__(self, mapping: MutableMapping) -> None:
+        self._mapping = mapping
+
+    def emit(self, *args: Any, who: Publisher) -> None:
+        assert isinstance(who, Topic)
+        if len(args) == 1:
+            self._mapping[who.path] = args[0]
+        else:
+            self._mapping[who.path] = args
