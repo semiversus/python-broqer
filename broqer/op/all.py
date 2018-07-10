5ed38c98c33f8e74ea6550073c91981286c252d1
Günther Jena <guenther@jena.at>
2018-07-10 02:19:31 +0200

    added python 3.7 for travis tests

--- broqer/op/all.py
+++
@@ -1,9 +0,0 @@
-from .any import _MultiPredicate
-from ._operator import build_operator
-
-
-class All(_MultiPredicate):
-    combination_operator = all  # type: ignore
-
-
-all = build_operator(All)
