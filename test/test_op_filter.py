f0d0bf305f983b97ba5a566c365f5eca98b95b0d
Günther Jena <guenther@jena.at>
2018-07-18 21:30:21 +0200

    added test for op.Filter

---
+++ test/test_op_filter.py
@@ -0,0 +1,15 @@
+from unittest import mock
+import pytest
+
+from broqer.op import Filter
+
+from .helper import check_single_operator, NONE
+
+@pytest.mark.parametrize('args, kwargs, input_vector, output_vector', [
+    ((lambda v:v==0,), {}, (1, 2, 0, 0.0, None), (NONE, NONE, 0, 0.0, NONE)),
+    ((), {'predicate': lambda v:v==0}, (1, 2, 0, 0.0, None), (NONE, NONE, 0, 0.0, NONE)),
+    ((lambda v,a:v==a, 0), {}, (1, 2, 0, 0.0, None), (NONE, NONE, 0, 0.0, NONE)),
+    ((lambda v,a:v==a, 1), {}, (1, 2, 0, 0.0, None), (1, NONE, NONE, NONE, NONE)),
+])
+def test_with_publisher(args, kwargs, input_vector, output_vector):
+    check_single_operator(Filter, args, kwargs, input_vector, output_vector)
