2071299d7e0b2a55d5d251505e17226992376963
Günther Jena <guenther@jena.at>
2018-07-22 00:30:04 +0200

    move position of default_error_handler import

--- broqer/__init__.py
+++ broqer/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
+from .default_error_handler import default_error_handler
 from .core import StatefulPublisher, Disposable, Publisher, Subscriber, \
                   SubscriptionDisposable, SubscriptionError, to_args, \
                   unpack_args
 
 from .hub import Hub, SubHub
 from .subject import Subject, Value
-from .default_error_handler import default_error_handler
 
 __author__ = 'Günther Jena'
 __email__ = 'guenther@jena.at'
