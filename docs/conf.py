cdc88419e9af126f70a40e5803247faed72b96d5
Günther Jena <guenther@jena.at>
2018-07-25 14:54:28 +0200

    Bump version: 0.4.0-dev → 0.4.1-dev

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'broqer'
 copyright = u'2018, Günther Jena'
 author = u'Günther Jena'
-version = '0.4.0-dev'
+version = '0.4.1-dev'
 
 extensions = [
     'sphinx.ext.autodoc',
