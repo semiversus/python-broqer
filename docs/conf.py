635ed551ad275eccbd4fc04d589cf41c64edf2b6
Günther Jena <guenther@jena.at>
2018-08-05 20:45:08 +0200

    Bump version: 0.4.1 → 0.4.2-dev

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'broqer'
 copyright = u'2018, Günther Jena'
 author = u'Günther Jena'
-version = '0.4.1'
+version = '0.4.2-dev'
 
 extensions = [
     'sphinx.ext.autodoc',
--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/semiversus/python-broqer',
-    version='0.4.1',
+    version='0.4.2-dev',
     zip_safe=False,
 )
