0ab36bf8b27fd2ca3788aadc9aa753f679918346
Günther Jena <guenther@jena.at>
2018-08-22 23:04:49 +0200

    newlines for __init__.py

--- broqer/op/publishers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .from_iterable import FromIterable
-from .from_polling import FromPolling
-
-__all__ = [
-    'FromIterable', 'FromPolling'
-]
--- broqer/op/subscribers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .sink import Sink, sink
-from .to_future import ToFuture, to_future
-
-__all__ = [
-    'Sink', 'sink', 'ToFuture', 'to_future'
-]
