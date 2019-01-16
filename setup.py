34e93c461118942ac5b77dfa7abefbb8aeed89b2
Günther Jena <guenther@jena.at>
2019-01-16 10:53:47 +0100

    update "Development Status" in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author="Günther Jena",
     author_email='guenther@jena.at',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
