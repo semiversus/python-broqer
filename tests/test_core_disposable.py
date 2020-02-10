791628dea7afd6aba5911e6475d7a79318e9d4cd
Günther Jena <guenther@jena.at>
2020-02-10 05:37:42 +0100

    extended Disposable tests

--- tests/test_core_disposable.py
+++ tests/test_core_disposable.py
@@ -1,4 +1,39 @@
-from broqer import SubscriptionDisposable, Publisher, Value
+from unittest import mock
+
+from broqer import Disposable, SubscriptionDisposable, Publisher, Value
+
+
+def test_disposable():
+    """ Testing base class Disposable
+    """
+    m = mock.Mock()
+
+    class MyDisposable(Disposable):
+        def dispose(self):
+            m('disposed')
+
+    d = MyDisposable()
+
+    # test simple .dispose() call
+    m.assert_not_called()
+    d.dispose()
+    m.assert_called_once_with('disposed')
+
+    m.reset_mock()
+
+    # test context manager
+    with d:
+        m.assert_not_called()
+
+    m.assert_called_once_with('disposed')
+
+    m.reset_mock()
+
+    # test context manager creating the disposable
+    with MyDisposable() as d2:
+        m.assert_not_called()
+
+    m.assert_called_once_with('disposed')
 
 
 def test_subscription_disposable():
