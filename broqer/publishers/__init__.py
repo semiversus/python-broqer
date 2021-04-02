a285692740763e3da4d63d64fa648d36e02c5225
Günther Jena <guenther@jena.at>
2021-04-02 09:55:53 +0200

    fixed linter problems

--- broqer/publishers/__init__.py
+++ broqer/publishers/__init__.py
@@ -1,2 +1,4 @@
 """ Module containing Publishers """
 from .poll import PollPublisher
+
+__all__ = ['PollPublisher']
--- broqer/publishers/poll.py
+++ broqer/publishers/poll.py
@@ -1,6 +1,6 @@
 """ Implementing PollPublisher """
 import asyncio
-from typing import Callable, Type, Callable, Any
+from typing import Callable, Type, Any, Optional
 
 from broqer.publisher import Publisher, TValue, SubscriptionDisposable
 from broqer.subscriber import Subscriber
@@ -8,8 +8,8 @@ from broqer.subscriber import Subscriber
 
 class PollPublisher(Publisher):
     """ A PollPublisher is periodically calling a given function and is using
-    the returned value to notify the subscribers. When no subscriber is present,
-    the polling function is not called.
+    the returned value to notify the subscribers. When no subscriber is
+    present, the polling function is not called.
 
     :param poll_cb: Function to be called
     :param interval: Time in seconds between polling calls
@@ -19,10 +19,10 @@ class PollPublisher(Publisher):
         Publisher.__init__(self, type_=type_)
         self.poll_cb = poll_cb
         self.interval = interval
-        self._poll_handler = None
+        self._poll_handler = None  # type: Optional[asyncio.TimerHandle]
 
-    def subscribe(self, subscriber: 'Subscriber',
-                  prepend: bool = False) -> 'SubscriptionDisposable':
+    def subscribe(self, subscriber: Subscriber,
+                  prepend: bool = False) -> SubscriptionDisposable:
         if not self._subscriptions:
             # call poll_cb once to set internal state and schedule a _poll call
             self._state = self.poll_cb()
@@ -34,7 +34,7 @@ class PollPublisher(Publisher):
 
     def unsubscribe(self, subscriber: 'Subscriber') -> None:
         Publisher.unsubscribe(self, subscriber)
-        if not self._subscriptions:
+        if not self._subscriptions and self._poll_handler:
             # when no subscription is left, cancel next _poll and reset state
             self._poll_handler.cancel()
             self._poll_handler = None
